f9f1fe54e3b1d1fdcc66980d17f6767e6abac7d7
horiidonuts <eray.tuna1221@gmail.com>
2024-11-30 22:47:49 +0300

    Completed score data storing

--- NumberGuessingGame/NumberGuessingGame/NumberGuessingGame.cpp
+++ NumberGuessingGame/NumberGuessingGame/NumberGuessingGame.cpp
@@ -6,8 +6,10 @@
 #include <string.h>
 #define welcome_length 5
 #define info_length 8
-#define select_1_length 6
-//#define _CRT_SECURE_NO_WARNINGS
+#define select_1_length 7
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+#define _CRT_SECURE_NO_DEPRECATE
 
 // input olarak i girildiginde puan karsiligi bilgi ipucu alabilme ozelligi
 
@@ -20,16 +22,15 @@ bool singleShot = false;
 int playerGuess;
 int totalPoints = 0;
 int currentSessionPoints = 0;
-char buffer[10];
-
+int sessionPoints = 0;
 
 char welcome_message[welcome_length][100] = 
 { 
 	{"Welcome to the number guessing game!\n"}, 
 	{"Please select a mode to play\n"},
-	{"1: You guess and computer chooses\n"}, 
-	{"2: You choose and computer guesses\n"}, 
-	{"3: Read how scoring is calculated\n"} 
+	{"1: Start guessing numbers.\n"}, 
+	{"2: Check your total and current score for this session.\n"}, 
+	{"3: Read how scoring is calculated.\n"} 
 };
 
 char score_info_message[info_length][100] = 
@@ -51,7 +52,8 @@ char selection_1[select_1_length][100] =
 	{"'M' (Medium): Guess in a range from 0 to 100\n"},
 	{"'H' (hard): Guess in a range from 0 to 250\n"},
 	{"'X' to exit to the main menu.\n(The menu accepts lowercase characters)\n"},
-	{"\nTotal points achieved: "}
+	{"Total points achieved: "},
+	{"\nCurrent points achieved for this session: "}
 };
 
 char hasGuessed[3][100] =
@@ -63,7 +65,12 @@ char hasGuessed[3][100] =
 
 char errorMessage[1][100] =
 {
-	{"Invalid selection. Please Select again."}
+	{"\nInvalid selection. Please Select again.\n"}
+};
+
+char returnMessage[1][100] =
+{
+	{"Press any key to return to the main menu.\n"}
 };
 
 void TypeWriter(char text_array[][100], int array_length, bool typeOnce, int index)
@@ -90,18 +97,36 @@ void TypeWriter(char text_array[][100], int array_length, bool typeOnce, int ind
 	}
 }
 
-int main()
+void ClearBuffer()
 {
+	int ch;
+	while ((ch = getchar()) != '\n' && ch != EOF);
+}
+
+void WriteScore(int score)
+{
+	char buffer[20];
 	FILE* scoredata;
 	errno_t err;
-	err = fopen_s(&scoredata, "scoredata.txt", "w");
+	err =  fopen_s(&scoredata, "scoredata.txt", "w");
+	_itoa_s(score, buffer, 10);
+	fprintf(scoredata, buffer);
+	fclose(scoredata);
+}
 
-	if (err != 0)
-	{
-		printf("Failed to open the file. Error code: %d\n", err);
-		return 1;
-	}
+int ReadScore()
+{
+	char buffer[20];
+	FILE* scoredata;
+	errno_t err;
+	err = fopen_s(&scoredata, "scoredata.txt", "r");
+	totalPoints = atoi(fgets(buffer,20,scoredata));
+	fclose(scoredata);
+	return totalPoints;
+}
 
+int main()
+{	
 	while (true) // main menu loop
 	{
 		difficultySelection = ' ';
@@ -118,11 +143,12 @@ int main()
 				if (difficultySelection == 'X' || difficultySelection == 'x') { break; }
 				system("cls");
 				TypeWriter(selection_1, 5, false, 0);
-				printf("%d\n", totalPoints);
+				printf("%d\n", ReadScore());
+				TypeWriter(selection_1, 1, true, 6);
+				printf("%d\n", sessionPoints);
 				printf("\nDifficulty > ");
 
-				int ch;
-				while ((ch = getchar()) != '\n' && ch != EOF);
+				ClearBuffer();
 
 				scanf_s("%c", &difficultySelection, 1);
 
@@ -143,7 +169,6 @@ int main()
 				}
 				else if (difficultySelection == 'X' || difficultySelection == 'x')
 				{
-					//fclose_s(*scoredata);
 					continue;
 				}
 				else
@@ -152,14 +177,43 @@ int main()
 					system("PAUSE");
 					continue;
 				}
+
+				selectedNumber = rand() % (upperBound - 0 + 1);
+				system("cls");
+				printf("Current Number: %d\n", selectedNumber);
+				TypeWriter(hasGuessed, 1, true, rand() % 3);
 			}
-			selectedNumber = rand() % (upperBound - 0 + 1);
-			system("cls");
-			printf("Current Number: %d\n", selectedNumber);
-			TypeWriter(hasGuessed, 1, true, rand() % 3);
-		
+
+			else if (menuSelection == 2)
+			{
+				system("cls");
+				TypeWriter(selection_1, 1, true, 5);
+				printf("%d\n", ReadScore());
+				TypeWriter(selection_1, 1, true, 6);
+				printf("%d\n", currentPoints);
+				system("PAUSE");
+				break;
+			}
+			
+			else if (menuSelection == 3)
+			{
+				system("cls");
+				TypeWriter(score_info_message, info_length - 1, false, 0);
+				TypeWriter(returnMessage, 1, false, 0);
+				system("PAUSE");
+				break;
+			}
+
+			else 
+			{ 
+				TypeWriter(errorMessage, 1, false, 0);
+				system("PAUSE");
+				break;
+			}
+			
 			while (menuSelection == 1) // game loop
 			{
+				
 				printf("\nGuess > ");
 				int ch;
 				while ((ch = getchar()) != '\n' && ch != EOF);
@@ -167,11 +221,14 @@ int main()
 
 				if (playerGuess == selectedNumber)
 				{
+					char buffer[20];
 					system("cls");
 					printf("Correct Guess! Current Points Achieved: %d\n", currentPoints);
 					totalPoints += currentPoints;
-					//itoa(difficultySelection, buffer, 10);
-					//fprintf(scoredata, buffer);
+					sessionPoints += currentPoints;
+					//totalPoints += currentPoints;
+					WriteScore(totalPoints);
+					system("PAUSE");
 					break;
 				}
 				else
@@ -184,7 +241,7 @@ int main()
 				
 			}
 
-			system("PAUSE");
+			//system("PAUSE");
 
 		}
 	}
